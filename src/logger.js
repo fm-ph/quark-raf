f0aaec527dc84165eafc552aeec557711368b503
Fabien Motte <fabien.motte@makemepulse.com>
2017-10-18 12:12:37 +0200

    fix: remove logger package import

--- src/logger.js
+++ src/logger.js
@@ -1,7 +1,6 @@
 import { Logger, plugins } from 'quark-log'
-import { name } from '../package.json'
 
 const logger = new Logger()
-logger.plugin('namespace', plugins.namespace, { name })
+logger.plugin('namespace', plugins.namespace, { name: 'quark-raf' })
 
 export default logger
